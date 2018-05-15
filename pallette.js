fedc7e65cdfbb9a185936022d85cc24c4f75df44
Paul Miller <paul@paulmillr.com>
2018-05-14 18:32:29 -0700

    Copy files

---
+++ pallette.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */
+const pallette = [  
+    '#ffffff',
+    '#ff0000',
+    '#ff8000',
+    '#ffff00',
+    '#80ff00',
+    '#00ff00',
+    '#00ff80',
+    '#00ffff',
+    '#0080ff',
+    '#0000ff',
+    '#8000ff',
+    '#ff00ff',
+    '#ff0080',
+    '#000000'
+ ];
---
+++ start.sh
@@ -0,0 +1,1 @@
+sudo gunicorn wsgi:app --worker-class eventlet -w 1 --bind 0.0.0.0:5000 --reload --daemon
